e5bc0f7a33ea0aaffbf5b09d385abcc0bb05c971
dung.nguyentrung <dung.nguyentrung@ncc.asia>
2023-08-08 03:05:31 +0700

    Add base

--- src/java/DAOS/CrewDAO.java
+++ src/java/DAOS/CrewDAO.java
@@ -5,10 +5,30 @@
  */
 package DAOS;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Crew;
+
 /**
  *
  * @author Administrator
  */
-public class CrewDAO {
-    
+public class CrewDAO extends DataAccessObject {
+    public List<Crew> GetAll() throws SQLException {
+        List<Crew> crews = new ArrayList<>();
+        String sql = "Select ID, Name, PhoneNumber, Position From Crew";
+        preparedStatement = connection.prepareStatement(sql);
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()){
+            Crew crew = new Crew();
+            crew.setID(resultSet.getInt("ID"));
+            crew.setName(resultSet.getString("Name"));
+            crew.setPhoneNumber(resultSet.getString("PhoneNumber"));
+            crew.setPosition(resultSet.getString("Position"));
+            crews.add(crew);
+        }
+        return crews;
+    }
 }
