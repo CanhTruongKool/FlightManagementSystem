f9c21482a7a1b0439da419100bca16f7e8d81ae0
CanhTruongKool <120951113+CanhTruongKool@users.noreply.github.com>
2023-08-07 22:32:39 +0700

    Update Crew.java

--- src/java/Model/Crew.java
+++ src/java/Model/Crew.java
@@ -9,6 +9,66 @@ package Model;
  *
  * @author Administrator
  */
-public class Crew {
+public class Cargo {
+    private int FlightID, PassengerID;
+    private String Category;
+    private float Weight, Price;
+
+    public Cargo() {
+    }
+
+    public Cargo(int FlightID, int PassengerID, String Category, float Weight, float Price) {
+        this.FlightID = FlightID;
+        this.PassengerID = PassengerID;
+        this.Category = Category;
+        this.Weight = Weight;
+        this.Price = Price;
+    }
+
+    public int getFlightID() {
+        return FlightID;
+    }
+
+    public void setFlightID(int FlightID) {
+        this.FlightID = FlightID;
+    }
+
+    public int getPassengerID() {
+        return PassengerID;
+    }
+
+    public void setPassengerID(int PassengerID) {
+        this.PassengerID = PassengerID;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String Category) {
+        this.Category = Category;
+    }
+
+    public float getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(float Weight) {
+        this.Weight = Weight;
+    }
+
+    public float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(float Price) {
+        this.Price = Price;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargo{" + "FlightID=" + FlightID + ", PassengerID=" + PassengerID + ", Category=" + Category + ", Weight=" + Weight + ", Price=" + Price + '}';
+    }
+    
     
 }
