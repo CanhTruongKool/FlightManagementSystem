15eb5f218e88619ab0567f048ff5ed51c7aecf2d
CanhTruongKool <120951113+CanhTruongKool@users.noreply.github.com>
2023-08-07 22:22:09 +0700

    Update Passenger.java

--- src/java/Model/Passenger.java
+++ src/java/Model/Passenger.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author Administrator
- */
 public class Passenger {
+    private String Name,PhoneNumber,IdentityNumber;
+
+    public Passenger() {
+    }
+
+    public Passenger(String Name, String PhoneNumber, String IdentityNumber) {
+        this.Name = Name;
+        this.PhoneNumber = PhoneNumber;
+        this.IdentityNumber = IdentityNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getIdentityNumber() {
+        return IdentityNumber;
+    }
+
+    public void setIdentityNumber(String IdentityNumber) {
+        this.IdentityNumber = IdentityNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" + "Name=" + Name + ", PhoneNumber=" + PhoneNumber + ", IdentityNumber=" + IdentityNumber + '}';
+    }
+    
     
 }
